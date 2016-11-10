f377cf20788d8f6a17fc66998b9118fc0e2c795e
Brian Smith <brian.smith.keith@gmail.com>
2016-11-09 22:59:11 -0500

    added v2 endpoint

--- jloser/config/routes.rb
+++ jloser/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'images/new'
+  get '/v2', to: 'pages#v2'
 
+  get 'images/new'
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
