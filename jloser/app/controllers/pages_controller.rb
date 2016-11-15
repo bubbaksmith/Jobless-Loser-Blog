138ea52713438e7206f68180336b8acf68997df8
Brian Smith <briansmith@Brians-MBP.attlocal.net>
2016-11-14 19:30:29 -0500

    added yaml files and erb for easier editing

--- jloser/app/controllers/pages_controller.rb
+++ jloser/app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @tabs = ['#home','#about','#services','/blog','#contact']
+    @about_text = YAML.load_file("#{Rails.root.to_s}/app/config/home.yml")['about']
+    @services_text = YAML.load_file("#{Rails.root.to_s}/app/config/home.yml")['services']
   end
 end
