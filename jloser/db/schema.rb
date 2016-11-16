264e23fb5e21040a51c5cea787ff135e6b458819
Brian Smith <brian.smith.keith@gmail.com>
2016-11-15 21:09:04 -0500

    testing cron

--- jloser/db/schema.rb
+++ jloser/db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108221639) do
+ActiveRecord::Schema.define(version: 20161116011519) do
 
   create_table "images", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image",      limit: 255
+    t.string   "blurb",      limit: 255
   end
 
   create_table "users", force: :cascade do |t|
